9096be3d480a8dbd8a8677b09d11cc4c999f81d4
pskirank <38557150+pskirank@users.noreply.github.com>
2018-12-22 11:50:55 +0530

    Add files via upload

---
+++ string1.py
@@ -0,0 +1,18 @@
+import string
+
+def swap_case(s):
+    return s.swapcase()
+def split_and_join():
+    line = line.split()
+    line = "-".join(line)
+    return line
+def
+
+
+if __name__ == '__main__':
+    s = input("Enter the string:")
+    swap_result = swap_case(s)
+    print(swap_result)
+    print(split_and_join())
+
+print(split_and_join())
