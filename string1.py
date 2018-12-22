694a59d7aff519a02431bbc1ef4329e63c31dca1
pskirank <38557150+pskirank@users.noreply.github.com>
2018-12-22 11:52:04 +0530

    Update string1.py

--- string1.py
+++ string1.py
@@ -6,13 +6,9 @@ def split_and_join():
     line = line.split()
     line = "-".join(line)
     return line
-def
-
 
 if __name__ == '__main__':
     s = input("Enter the string:")
     swap_result = swap_case(s)
     print(swap_result)
     print(split_and_join())
-
-print(split_and_join())
