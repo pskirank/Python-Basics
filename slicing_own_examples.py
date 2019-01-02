a577a2897e2d31c4e67e38908d72dea7bac2b63c
pskirank <38557150+pskirank@users.noreply.github.com>
2019-01-02 20:47:09 +0530

    Create slicing_own_examples.py

---
+++ slicing_own_examples.py
@@ -0,0 +1,14 @@
+"""
+My own examples to understand slicing in strings
+"""
+s = input("Enter string:")
+#Printing Everything
+print(s[:])
+#Printing upto 4 characters from beginning
+print(s[:4])
+#starting from second (s[1]) print every 3rd character
+print(s[1::3])
+#Starting from 4th character print every 7th character
+print(s[4::7])
+#starting from 3r character (s[2]) print till 8th character
+print(s[2:8])
