84c1a9571549b7dbaa8c297fb0fb34b22901e320
pskirank <38557150+pskirank@users.noreply.github.com>
2023-05-16 09:57:38 +0530

    Add files via upload

---
+++ floor_and_ceik.py
@@ -0,0 +1,9 @@
+#https://www.hackerrank.com/challenges/floor-ceil-and-rint?isFullScreen=true
+
+import numpy
+numpy.set_printoptions(legacy='1.13')
+
+my_array = numpy.array(list(map(float,input().split())))
+print(numpy.floor(my_array))
+print(numpy.ceil(my_array))
+print(numpy.rint(my_array))
