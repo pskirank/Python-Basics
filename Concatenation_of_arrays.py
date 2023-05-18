759d1cd4a58a791e1245c27df1736343a63afd6a
pskirank <38557150+pskirank@users.noreply.github.com>
2023-05-18 21:35:50 +0530

    Add files via upload

---
+++ Concatenation_of_arrays.py
@@ -0,0 +1,7 @@
+#https://www.hackerrank.com/challenges/np-concatenate/problem?h_r=next-challenge&h_r=next-challenge&h_r=next-challenge&h_r=next-challenge&h_r=next-challenge&h_r=next-challenge&h_r=next-challenge&h_r=next-challenge&h_r=next-challenge&h_r=next-challenge&h_v=zen&h_v=zen&h_v=zen&h_v=zen&h_v=zen&h_v=zen&h_v=zen&h_v=zen&h_v=zen&h_v=zen&isFullScreen=true&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+
+import numpy
+n,m,p = map(int,input().split())
+arr1 = numpy.array([input().split() for i in range(n)], int)
+arr2 = numpy.array([input().split() for i in range(m)], int)
+print(numpy.concatenate((arr1, arr2)))
