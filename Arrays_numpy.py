d221158156af4abaf505780785b88482b896a2f7
pskirank <38557150+pskirank@users.noreply.github.com>
2023-05-18 22:09:15 +0530

    Add files via upload

---
+++ Arrays_numpy.py
@@ -0,0 +1,12 @@
+#https://www.hackerrank.com/challenges/np-arrays/problem?h_r=next-challenge&h_r=next-challenge&h_r=next-challenge&h_r=next-challenge&h_r=next-challenge&h_r=next-challenge&h_r=next-challenge&h_r=next-challenge&h_r=next-challenge&h_r=next-challenge&h_v=zen&h_v=zen&h_v=zen&h_v=zen&h_v=zen&h_v=zen&h_v=zen&h_v=zen&h_v=zen&h_v=zen&isFullScreen=true&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+
+import numpy
+
+def arrays(arr):
+    # complete this function
+    # use numpy.array
+    return numpy.fromiter(arr[::-1], dtype=float)
+
+arr = input().strip().split(' ')
+result = arrays(arr)
+print(result)
