45102d0410336ca23edc3b345dca8195dceece68
pskirank <38557150+pskirank@users.noreply.github.com>
2023-05-18 22:23:53 +0530

    Add files via upload

---
+++ shape_and_reshape.py
@@ -0,0 +1,6 @@
+#https://www.hackerrank.com/challenges/np-shape-reshape/problem?h_r=next-challenge&h_r=next-challenge&h_r=next-challenge&h_r=next-challenge&h_r=next-challenge&h_r=next-challenge&h_r=next-challenge&h_r=next-challenge&h_r=next-challenge&h_r=next-challenge&h_v=zen&h_v=zen&h_v=zen&h_v=zen&h_v=zen&h_v=zen&h_v=zen&h_v=zen&h_v=zen&h_v=zen&isFullScreen=true&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+
+import numpy
+
+list1 = list(map(int,input().split()))
+print(numpy.reshape(list1, (3,3)))
