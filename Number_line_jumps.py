6a70141f67a47d0335935cefcd86605af50937b0
pskirank <38557150+pskirank@users.noreply.github.com>
2023-05-20 14:48:32 +0530

    Add files via upload

---
+++ Number_line_jumps.py
@@ -0,0 +1,38 @@
+#https://www.hackerrank.com/challenges/kangaroo/problem?isFullScreen=true
+
+import math
+import os
+import random
+import re
+import sys
+
+
+# Complete the 'kangaroo' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts following parameters:
+#  1. INTEGER x1
+#  2. INTEGER v1
+#  3. INTEGER x2
+#  4. INTEGER v2
+#
+
+def kangaroo(x1, v1, x2, v2):
+    # Write your code here
+    try:
+        r = (x1 - x2) / (v2 - v1)
+        return 'YES' if r > 0 and r.is_integer() else 'NO'
+    except Exception:
+        return 'NO'
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    first_multiple_input = input().rstrip().split()
+    x1 = int(first_multiple_input[0])
+    v1 = int(first_multiple_input[1])
+    x2 = int(first_multiple_input[2])
+    v2 = int(first_multiple_input[3])
+    result = kangaroo(x1, v1, x2, v2)
+    fptr.write(result + '\n')
+    fptr.close()
