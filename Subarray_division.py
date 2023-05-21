bc84ea197c736395616aac07fe84883048bb0088
pskirank <38557150+pskirank@users.noreply.github.com>
2023-05-21 12:58:19 +0530

    Add files via upload

---
+++ Subarray_division.py
@@ -0,0 +1,40 @@
+#https://www.hackerrank.com/challenges/the-birthday-bar/problem?h_r=next-challenge&h_r=next-challenge&h_v=zen&h_v=zen&isFullScreen=true&h_r=next-challenge&h_v=zen
+
+# !/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+#
+# Complete the 'birthday' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY s
+#  2. INTEGER d
+#  3. INTEGER m
+#
+
+def birthday(s, d, m):
+    # Write your code here
+    cnt = 0
+    for i in range(len(s) - m + 1):
+        if sum(s[i:i + m]) == d:
+            cnt += 1
+    return cnt
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    n = int(input().strip())
+    s = list(map(int, input().rstrip().split()))
+    first_multiple_input = input().rstrip().split()
+    d = int(first_multiple_input[0])
+    m = int(first_multiple_input[1])
+    result = birthday(s, d, m)
+    fptr.write(str(result) + '\n')
+    fptr.close()
