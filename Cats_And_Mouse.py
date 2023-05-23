77d0c852013e8db5de15490cee4d9b65c7327a07
pskirank <38557150+pskirank@users.noreply.github.com>
2023-05-23 23:58:35 +0530

    Add files via upload

---
+++ Cats_And_Mouse.py
@@ -0,0 +1,32 @@
+#https://www.hackerrank.com/challenges/cats-and-a-mouse/problem?h_r=next-challenge&h_r=next-challenge&h_r=next-challenge&h_r=next-challenge&h_r=next-challenge&h_r=next-challenge&h_r=next-challenge&h_v=zen&h_v=zen&h_v=zen&h_v=zen&h_v=zen&h_v=zen&h_v=zen&isFullScreen=true&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+
+# !/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+# Complete the catAndMouse function below.
+def catAndMouse(x, y, z):
+    if abs(z - x) < abs(z - y):
+        return "Cat A"
+    elif abs(z - x) == abs(z - y):
+        return "Mouse C"
+    else:
+        return "Cat B"
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    q = int(input())
+    for q_itr in range(q):
+        xyz = input().split()
+        x = int(xyz[0])
+        y = int(xyz[1])
+        z = int(xyz[2])
+        result = catAndMouse(x, y, z)
+        fptr.write(result + '\n')
+    fptr.close()
