34d16184efac1980242f13a0d41771fe7b141007
pskirank <38557150+pskirank@users.noreply.github.com>
2023-06-07 23:42:44 +0530

    Add files via upload

---
+++ kata_book_seller.py
@@ -0,0 +1,19 @@
+# https://www.codewars.com/kata/54dc6f5a224c26032800005c/train/python
+
+from collections import Counter
+
+def stock_list(listOfArt, listOfCat):
+    if not listOfArt:
+        return ''
+    codePos = listOfArt[0].index(' ') + 1
+    cnt = Counter()
+    for s in listOfArt:
+        cnt[s[0]] += int(s[codePos:])
+    return ' - '.join('({} : {})'.format(cat, cnt[cat]) for cat in listOfCat)
+
+b = ["ABAR 200", "CDXE 500", "BKWR 250", "BTSQ 890", "DRTY 600"]
+c = ["A", "B", "C"]
+
+print stock_list(b, c)
+
+# -- Result -- Couldn't be solved - Answer copied from an existing solution
