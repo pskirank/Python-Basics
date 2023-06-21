bd0e33bd82844bf61279023796ac0ffea3778621
pskirank <38557150+pskirank@users.noreply.github.com>
2023-06-21 22:17:30 +0530

    Add files via upload

--- sherlock_and_valid_String.py
+++ sherlock_and_valid_String.py
@@ -1,5 +1,60 @@
 #https://www.hackerrank.com/challenges/sherlock-and-valid-string/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=strings&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
 
+#Solution-1
+
+# !/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+#
+# Complete the 'isValid' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING s as parameter.
+#
+
+def isValid(s):
+    # Write your code here
+    freq = {}
+
+    for c in s:
+        if freq.get(c):
+            freq[c] += 1
+        else:
+            freq[c] = 1
+
+    frequencies = list(freq.values())
+    prev_frequency = frequencies[0]
+
+    removed_character = False
+
+    for i in range(1, len(frequencies)):
+        if prev_frequency != frequencies[i]:
+            if removed_character:
+                return 'NO'
+            else:
+                removed_character = True
+    return 'YES'
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = isValid(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
+
+#Solution-2
+
 # !/bin/python3
 
 import math
